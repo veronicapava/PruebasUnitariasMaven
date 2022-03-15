218b0c16c7c251f374d25fa5fb244fb9a91ad78f
veronicapava <veronicapava@gmail.com>
2022-03-14 20:36:37 -0500

    Crea metodo para sumar y clase para ejecutar la aplicacion

---
+++ src/main/java/co/com/sofka/app/calculator/BasicCalculator.java
@@ -0,0 +1,17 @@
+package co.com.sofka.app.calculator;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BasicCalculator {
+
+    private static final Logger logger = LoggerFactory.getLogger(BasicCalculator.class);
+
+    //TODO: Completar las demás operaciones aritméticas.
+
+    public Long sum(Long number1, Long number2) {
+        logger.info( "Summing {} + {}", number1, number2 );
+        return number1 + number2;
+    }
+
+}
