8f433568a91b363f3ddb9927e73440c55b19e0fe
veronicapava <veronicapava@gmail.com>
2022-03-14 21:59:40 -0500

    Crea otro Test para Multiplicacion, con mas casos de prueba

--- src/test/java/BasicCalculatorMultiTest.java
+++ src/test/java/BasicCalculatorMultiTest.java
@@ -2,6 +2,10 @@ import co.com.sofka.app.calculator.BasicCalculator;
 import junit.framework.Assert;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BasicCalculatorMultiTest {
 
@@ -22,4 +26,20 @@ public class BasicCalculatorMultiTest {
         Assert.assertEquals(expectedValue, result);
 
     }
+
+    //Probando la lógica con muchos más casos de prueba
+    @DisplayName("Testing several rest")
+    @ParameterizedTest(name = "{0} * {1} = {2}")
+    @CsvSource({
+            "3,    5,   15",
+            "5,    2,   10",
+            "30,  30, 900",
+            "26,  85, 2210"
+    })
+    public void severalSums(Long first, Long second, Long expectedResult) {
+        assertEquals(expectedResult, basicCalculator.mult(first, second),
+                () -> first + " * " + second + " should equal " + expectedResult);
+    }
+
+
 }
