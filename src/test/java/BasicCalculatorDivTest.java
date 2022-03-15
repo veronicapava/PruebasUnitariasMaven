40a65a575564f274a8a6a2e345aa74b4d8dbb3b9
veronicapava <veronicapava@gmail.com>
2022-03-14 22:04:41 -0500

    Crea otro Test para Division, con muchos mas casos de prueba

--- src/test/java/BasicCalculatorDivTest.java
+++ src/test/java/BasicCalculatorDivTest.java
@@ -2,6 +2,10 @@ import co.com.sofka.app.calculator.BasicCalculator;
 import junit.framework.Assert;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BasicCalculatorDivTest {
     private final BasicCalculator basicCalculator = new BasicCalculator();
@@ -19,5 +23,19 @@ public class BasicCalculatorDivTest {
 
         // Assert
         Assert.assertEquals(expectedValue, result);
+    }
+
+    //Probando la lógica con muchos más casos de prueba
+    @DisplayName("Testing several divitiones")
+    @ParameterizedTest(name = "{0} / {1} = {2}")
+    @CsvSource({
+            "30,    2,   15",
+            "23,    2,   11,5",
+            "10,  10, 1",
+            "40,  2, 20"
+    })
+    public void severalDiv(Long first, Long second, Long expectedResult) {
+        assertEquals(expectedResult, basicCalculator.div(first, second),
+                () -> first + " / " + second + " should equal " + expectedResult);
     }
 }
